26eb43a3b0dbdda288ee1654be091b8cf996bbc8
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-05-15 10:39:50 +0200

    add ComponentArrays

--- src/RobustAndOptimalControl.jl
+++ src/RobustAndOptimalControl.jl
@@ -5,6 +5,7 @@ using RecipesBase
 using ControlSystems
 import ControlSystems: ss, ssdata, ninputs, noutputs, nstates, isdiscrete, iscontinuous, to_matrix, timeevol, _string_mat_with_headers, PartionedStateSpace, common_timeevol
 
+using ComponentArrays
 export ExtendedStateSpace
 
 export hinfsynthesize, hinfassumptions, hinfpartition, hinfsignals, bilinearc2d, bilineard2c, fudge_inv
