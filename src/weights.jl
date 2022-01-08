ca9f8843ff7696bed66e2d3a7f5692a35e1496d1
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-01-08 11:49:02 +0100

    add example in docstring

--- src/weights.jl
+++ src/weights.jl
@@ -8,6 +8,14 @@ Create a weighting function that goes from gain `low` at zero frequency, through
 - `low`: A number specifying the DC gain 
 - `mid`: A number specifying the frequency at which the gain is 1, or a tuple `(freq, gain)`. If `gain_mid` is not specified, the geometric mean of `high` and `low` is used.
 - `high`: A number specifying the gain at ∞
+
+```@example
+using ControlSystems, Plots
+W = makeweight(10, (5,2), 1/10)
+bodeplot(W)
+hline!([10, 2, 1/10], l=(:black, :dash), primary=false)
+vline!([5], l=(:black, :dash), primary=false)
+```
 """
 function makeweight(low, mid::Number, high)
     makeweight(low, (mid, high < 1 ? √(high*low) : 1), high)
