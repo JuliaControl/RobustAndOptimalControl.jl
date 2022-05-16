53904b13f8e0f646fbee04aa98e1f7aa52c1cf20
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-05-16 13:48:04 +0200

    default color update

--- src/plotting.jl
+++ src/plotting.jl
@@ -9,6 +9,23 @@ the three weighting functions \$W_S(s), W_U(s), W_T(s)\$ inverted and scaled by 
 against the corresponding transfer functions \$S(s), C(s)S(s), T(s)\$, to
 verify visually that the specifications are met. This may be run using both MIMO
 and SISO systems.
+
+# Keyword args
+- `wint`: `(-3, 5)` frequency range (log10)
+- `wnum`: 101 number of frequency points
+- `hz`: true
+- `nsigma`: typemax(Int) number of singular values to show
+- `s_labels`: `[
+    "σ(S)",
+    "σ(CS)",
+    "σ(T)",
+]`
+- `w_labels`: `[
+    "γ σ(Wₛ⁻¹)",
+    "γ σ(Wᵤ⁻¹)",
+    "γ σ(Wₜ⁻¹)",
+]`
+- `colors`: `[:blue, :red, :green]` colors for ``S``, ``CS`` and ``T``
 """
 specificationplot
 @recipe function specificationplot(
@@ -27,7 +44,7 @@ specificationplot
         "γ σ(Wᵤ⁻¹)",
         "γ σ(Wₜ⁻¹)",
     ],
-    colors = [:red, :blue, :green],
+    colors = [:blue, :red, :green],
 )
 
     sensitivityfunctions = p.args[1]
