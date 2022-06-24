9cf0ff994635bef6ea37816447e24269c8dc01c9
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-06-24 08:24:00 +0200

    add note on positive feedback controller

--- src/reduction.jl
+++ src/reduction.jl
@@ -155,6 +155,8 @@ Minimize    ||(K-Kᵣ) W||∞ if out=false
 See Robust and Optimal Control Ch 19.1
 out indicates if the weight will be applied as output or input weight.
 
+This function expects a *positive feedback controller `K`.
+
 This method corresponds to the methods labelled SW1/SW2(SPA) in 
 Andreas Varga, "Controller Reduction Using Accuracy-Enhancing Methods"
 SW1 is the default method, corresponding to `out=true`.
