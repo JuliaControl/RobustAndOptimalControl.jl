f4c61568cc9b52d9241fcc7dfac4c47a35e485c4
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-09-29 05:56:41 +0200

    export and document `sys_from`particles`

--- src/uncertainty_interface.jl
+++ src/uncertainty_interface.jl
@@ -473,8 +473,14 @@ using MonteCarloMeasurements: vecindex
 
 """
     sys_from_particles(P, i)
+    sys_from_particles(P)
 
 Return the `i`th system from a system `P` with `Particles` coefficients.
+
+If called without an index, return a vector of systems, one for each possibly `i`.
+
+This function is used to convert from an uncertain representation using `Particles` to a "multi-model" representation using multiple `StateSpace` models.
+
 See also [`ss2particles`](@ref) and `MonteCarloMeasurements.nominal`.
 """
 function sys_from_particles(P, i)
