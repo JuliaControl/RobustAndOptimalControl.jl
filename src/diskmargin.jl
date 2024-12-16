4120b358399dc6bafd93884c61d1bec97c03d257
Alex Hirzel <alex@hirzel.us>
2024-12-16 10:49:48 -0500

    fix typo - s/pahse/phase/

--- src/diskmargin.jl
+++ src/diskmargin.jl
@@ -79,7 +79,7 @@ end
 """
     Disk
 
-Represents a perturbation disc in the complex plane. `Disk(0.5, 2)` represents all perturbations in the circle centered at 1.25 with radius 0.75, or in other words, a gain margin of 2 and a pahse margin of 36.9 degrees.
+Represents a perturbation disc in the complex plane. `Disk(0.5, 2)` represents all perturbations in the circle centered at 1.25 with radius 0.75, or in other words, a gain margin of 2 and a phase margin of 36.9 degrees.
 
 A disk can be converted to a Nyquist exclusion disk by `nyquist(disk)` and plotted using `plot(disk)`.
 
